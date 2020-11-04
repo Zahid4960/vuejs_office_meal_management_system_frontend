8e3040057b320dc9ad523293650d4e36ece7ee0f
zahid4960 <zahidcse4960@gmail.com>
2020-11-04 17:35:34 +0600

    making everything ready for work

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-<div></div>
+<SideBar />
 </template>
 
 <script>
+import SideBar from './views/sidebar_view'
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    SideBar
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import './assets/css/sidebar_style.css'
 
 createApp(App).mount('#app')
+
